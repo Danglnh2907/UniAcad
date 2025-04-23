c66e0b04b426c493f4e508200e61cfb3938fe31c
Nguyễn Hoàng Khải <khaiproject1234@gmail.com>
2025-04-23 19:03:16 +0700

    Refactor MailService to update recipient variables and remove unused email entries

---
+++ src/main/java/util/normalization/NormalizationService.java
@@ -0,0 +1,37 @@
+package util.normalization;
+
+import org.attoparser.trace.MarkupTraceEvent;
+
+public class NormalizationService {
+    private final StringBuilder sb;
+    public NormalizationService() {
+        sb = new StringBuilder();
+    }
+
+    public String normalizationFullName(String input) {
+        String[] words = input.trim().split("\\s+");
+        sb.setLength(0); // clear StringBuilder for reuse
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                sb.append(Character.toUpperCase(word.charAt(0)));
+                if (word.length() > 1) {
+                    sb.append(word.substring(1).toLowerCase());
+                }
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim(); // remove trailing space
+    }
+
+    public String normaliizationEmail(String input)
+    {
+        return input.trim();
+    }
+
+    public static void main (String[] args)
+    {
+        NormalizationService normalizationService = new NormalizationService();
+        System.out.println(normalizationService.normalizationFullName("    ABC      bad  am"));
+    }
+}
